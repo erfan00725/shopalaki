27b7248ac6fcb8ab71b84c24548f67f3a86cad8f
Mahdi <m09371840596mr@gmail.com>
2023-12-30 16:39:01 +0330

    project finished

--- shop/Form1.cs
+++ shop/Form1.cs
@@ -24,8 +24,8 @@ namespace shop
         private DataSet dataSet1 = new DataSet(), dataSet2 = new DataSet(), dataSet3 = new DataSet();
         private List<ComboBox> orderBoxes = new List<ComboBox>();
         private string query = "", Orders = "";
-        private const int ProductOrdersListXCONST = 125;
-        private int ProductOrdersListNum = 0, ProductOrdersListX = ProductOrdersListXCONST, ProductOrdersListY = 34;
+        private const int ProductOrdersListXCONST = 150;
+        private int ProductOrdersListNum = 0, ProductOrdersListX = ProductOrdersListXCONST, ProductOrdersListY = 47;
 
         private void Form1_Load(object sender, EventArgs e)
         {
